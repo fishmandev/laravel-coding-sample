aa3613213fd2de9e32284edd790803c22c6048ea
Dmitriy Fishman <fishman.code@gmail.com>
2022-07-15 06:44:59 +0300

    Added 'statuses' to the 'Post' model and defined $fillable var

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    const STATUS_DRAFT = 'draft';
+
+    const STATUS_PUBLISHED = 'published';
+
+    const STATUS_HIDDEN = 'hidden';
+
+    public static array $statuses = [
+        self::STATUS_DRAFT => 'Draft',
+        self::STATUS_PUBLISHED => 'Published',
+        self::STATUS_HIDDEN => 'Hidden',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'body',
+        'status',
+    ];
 }
