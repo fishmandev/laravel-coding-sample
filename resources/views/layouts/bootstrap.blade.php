9187801d5b5b6df9e66826a1a3543f28f216da82
Dmitriy Fishman <fishman.code@gmail.com>
2022-07-15 06:51:14 +0300

    Created a bootstrap layout

---
+++ resources/views/layouts/bootstrap.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel coding sample</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
